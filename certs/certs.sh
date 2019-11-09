ddb986c8982a32339bb697d42eb7ff51f26b79f3
webdimension <office@webdimension.jp>
2019-11-09 17:16:25 +0000

    Mod san

--- certs/certs.sh
+++ certs/certs.sh
@@ -6,7 +6,7 @@
 #. ../htdocs/.env
 CN=$1
 # 対話式で入力するdistinguished nameを一度に指定。コモンネームは第1引数
-SJ="/C=JP/ST=Tokyo/L=Shibuya-ku/O=laravel/OU=laravel/CN=$CN"
+SJ="/C=JP/ST=Tokyo/L=Shibuya-ku/O=webdimension/OU=webdimension/CN=$CN"
 
 # 秘密鍵を作成
 openssl genrsa  -out server.key 2048
@@ -20,9 +20,9 @@ openssl req -new -sha256 -key server.key -out server.csr -subj "$SJ"
 # CRT作成
 # openssl x509 -req -in ssl/server.csr -signkey ssl/server.key -sha256 -days 36500 -out ssl/server.crt
 FILE="san.txt"
-if [ -e ${FILE} ]; then
- rm  ${FILE}
-fi
-touch ${FILE}
-echo "subjectAltName = DNS:$CN" >> ${FILE}
+#if [ -e ${FILE} ]; then
+# rm  ${FILE}
+#fi
+#touch ${FILE}
+#echo "subjectAltName = DNS:$CN" >> ${FILE}
 openssl x509 -req -in server.csr -signkey server.key -sha256 -days 36500 -out server.crt -extfile ${FILE}
