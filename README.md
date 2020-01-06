7f0e9d9556ce65ba34645c931b8bbac94918afed
webdimension <office@webdimension.jp>
2020-01-06 17:03:34 +0900

    Fix dotfiles

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# docker_shared
+# Nginx Proxy (Shared network)
+## Used Images
+- nginx -proxy
+- nginx
+- schickling/mailcatcher 
+
+## Usage
+**Create Docker Network**
+```bbash
+docker network create nginx-proxy
+```
+**SSL Setting**
+```bash
+cp .env.sample .env
+```
+```bash
+vi .env
+# example
+COMPOSE_PROJECT_NAME=nginx-proxy
+HOSTNAME=docer.local
+CSR_ORGANIZATION=webdimension
+CSR_ORGANIZATION_UNIT=webdimension
+KEY_LENGYH=2048
+HASH_LENGTH=sha256
+EXPIRE_DAYS=36500
+SAN_FILE=san.txt
+OUTPUT_FILE_NAME=server
+```
+```bash
+cp certs/san.txt.sample certs/san.txt
+```
+```bash
+vi certs/san.txt
+example
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = *.local
+DNS.2 = docker.local
+...
+...
+many
+
+```
+**Create CSR**
+```bash
+cd certs
+./serts.sh
+
+# Created 
+# certs/OUTPUT_FILE_NAME.key
+# certs/OUTPUT_FILE_NAME.csr
+# certs/OUTPUT_FILE_NAME.crt
+
+# Add to Keychains (MAC)
+```
+**Start nginx-proxy**
+```bash
+docker-compose up -d
+```
--- certs/certs.sh
+++ certs/certs.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
- if [ $# -ne 1 ]; then
-   echo "usage: $0 <fqdn>"
-    exit 1;
- fi
-#. ../htdocs/.env
-CN=$1
-# 対話式で入力するdistinguished nameを一度に指定。コモンネームは第1引数
-SJ="/C=JP/ST=Tokyo/L=Shibuya-ku/O=webdimension/OU=webdimension/CN=$CN"
+source  ../.env
+SJ="/C=JP/ST=Tokyo/L=Shibuya-ku/O=${CSR_ORGANIZATION}/OU=${CSR_ORGANIZATION_UNIT}/CN=${HOSTNAME}"
 
-# 秘密鍵を作成
-openssl genrsa  -out server.key 2048
+openssl genrsa  -out ${OUTPUT_FILE_NAME}.key ${KEY_LENGYH}
 
-# CSR出力
-openssl req -new -sha256 -key server.key -out server.csr -subj "$SJ"
+openssl req -new -${HASH_LENGTH} -key ${OUTPUT_FILE_NAME}.key -out ${OUTPUT_FILE_NAME}.csr -subj "$SJ"
 
-# 確認のためにCSR内容を表示
-# openssl req -noout -text -in ssl/server.csr
-
-# CRT作成
-# openssl x509 -req -in ssl/server.csr -signkey ssl/server.key -sha256 -days 36500 -out ssl/server.crt
-FILE="san.txt"
-#if [ -e ${FILE} ]; then
-# rm  ${FILE}
-#fi
-#touch ${FILE}
-#echo "subjectAltName = DNS:$CN" >> ${FILE}
-openssl x509 -req -in server.csr -signkey server.key -sha256 -days 36500 -out server.crt -extfile ${FILE}
+openssl x509 -req -in ${OUTPUT_FILE_NAME}.csr -signkey ${OUTPUT_FILE_NAME}.key -sha256 -days ${EXPIRE_DAYS} -out ${OUTPUT_FILE_NAME}.crt -extfile ${SAN_FILE}
