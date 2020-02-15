1e00de9969edbf05ac67d2cfab6892ffa5b13844
webdimension <office@webdimension.jp>
2020-02-15 13:09:14 +0900

    Fix certs.sh with .env

--- certs/certs.sh
+++ certs/certs.sh
@@ -2,8 +2,8 @@
 source  ../.env
 SJ="/C=JP/ST=Tokyo/L=Shibuya-ku/O=${CSR_ORGANIZATION}/OU=${CSR_ORGANIZATION_UNIT}/CN=${HOSTNAME}"
 
-openssl genrsa  -out ${OUTPUT_FILE_NAME}.key ${KEY_LENGYH}
+openssl genrsa  -out ${CERTS_FILE_NAME}.key ${KEY_LENGYH}
 
-openssl req -new -${HASH_LENGTH} -key ${OUTPUT_FILE_NAME}.key -out ${OUTPUT_FILE_NAME}.csr -subj "$SJ"
+openssl req -new -${HASH_LENGTH} -key ${CERTS_FILE_NAME}.key -out ${CERTS_FILE_NAME}.csr -subj "$SJ"
 
-openssl x509 -req -in ${OUTPUT_FILE_NAME}.csr -signkey ${OUTPUT_FILE_NAME}.key -sha256 -days ${EXPIRE_DAYS} -out ${OUTPUT_FILE_NAME}.crt -extfile ${SAN_FILE}
+openssl x509 -req -in ${CERTS_FILE_NAME}.csr -signkey ${CERTS_FILE_NAME}.key -sha256 -days ${EXPIRE_DAYS} -out ${CERTS_FILE_NAME}.crt -extfile ${SAN_FILE}
